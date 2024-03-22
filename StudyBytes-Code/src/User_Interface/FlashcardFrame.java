69ea2f440260f83a2f78566a1bf899d44b9db4eb
dicarrillo <dicarrillo04@gmail.com>
2024-03-22 12:13:34 -0600

    Updated proportions for borders in FlashcardFrame

--- StudyBytes-Code/src/User_Interface/FlashcardFrame.java
+++ StudyBytes-Code/src/User_Interface/FlashcardFrame.java
@@ -7,30 +7,34 @@ public class FlashcardFrame extends JFrame
 {
     public FlashcardFrame()
     {
+        // Set up frame
         this.setTitle("Study Bytes");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(500, 500);
         this.setVisible(true);
         this.setLayout(new BorderLayout());
 
-        JPanel cardPanel = new JPanel();
+        // Create panels
+        JPanel cardPanel = new JPanel();    // Panel to display flashcards
         JPanel northPanel = new JPanel();
         JPanel southPanel = new JPanel();
         JPanel eastPanel = new JPanel();
         JPanel westPanel = new JPanel();
 
-        cardPanel.setPreferredSize(new Dimension(25, 25));
-        northPanel.setPreferredSize(new Dimension(100, 100));
-        southPanel.setPreferredSize(new Dimension(100, 100));
-        eastPanel.setPreferredSize(new Dimension(100, 100));
-        westPanel.setPreferredSize(new Dimension(100, 100));
+        // Set border panel sizes
+        northPanel.setPreferredSize(new Dimension(0, 200));
+        southPanel.setPreferredSize(new Dimension(0, 200));
+        eastPanel.setPreferredSize(new Dimension(300, 0));
+        westPanel.setPreferredSize(new Dimension(300, 0));
 
+        // Panel colors
         cardPanel.setBackground(Color.WHITE);
         northPanel.setBackground(Color.BLUE);
         southPanel.setBackground(Color.RED);
         eastPanel.setBackground(Color.GREEN);
         westPanel.setBackground(Color.PINK);
 
+        // Add panels to frame
         this.add(cardPanel, BorderLayout.CENTER);
         this.add(northPanel, BorderLayout.NORTH);
         this.add(southPanel, BorderLayout.SOUTH);
