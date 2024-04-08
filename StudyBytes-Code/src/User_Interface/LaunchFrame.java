c62d31fcfb8828b8bfbb26130b46f24db1bf5ce8
dicarrillo <dicarrillo04@gmail.com>
2024-04-07 20:57:13 -0600

    Delete button functionality added

--- StudyBytes-Code/src/User_Interface/LaunchFrame.java
+++ StudyBytes-Code/src/User_Interface/LaunchFrame.java
@@ -32,6 +32,11 @@ public class LaunchFrame extends JFrame implements ActionListener {
 
         this.add(editButton);
         this.add(viewButton);
+
+        if (deck.studySet.size() == 0)
+        {   // Deck is empty, remove viewButton functionality
+            viewButton.setEnabled(false);
+        }
     }
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == editButton){
